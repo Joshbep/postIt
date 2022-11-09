56d590c368c518ef12eb81eedc11427406ae03a4
jbep <joshuabemartin@gmail.com>
2022-11-08 20:49:10 -0500

    started styled header bar

--- src/components/top/Headerbar.js
+++ src/components/top/Headerbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Search, Person, Chat, Notifications } from "@material-ui/icons"
 import "./Headerbar.css"
 
 function Headerbar() {
@@ -7,8 +8,37 @@ function Headerbar() {
       <div className="headerLeft">
         <span className="appName">Post It</span>
       </div>
-      <div className="headercenter"></div>
-      <div className="headeright"></div>
+      <div className="headerCenter">
+        <div className="searchBar">
+          <Search />
+          <input placeholder="Search For Friends or posts" className="search"/>
+        </div>
+      </div>
+      <div className="headeRight">
+        <div className="headerBarLinks">
+          <span className="headerBarLink">Homepage</span>
+          <span className="headerBarLink">TimeLine</span>
+        </div>
+        <div className="headerBarIcons">
+          <div className="barIcon">
+            <Person />
+            <span className="barIconCounter">1</span>
+          </div>
+        </div>
+        <div className="headerBarIcons">
+          <div className="barIcon">
+            <Chat />
+            <span className="barIconCounter">1</span>
+          </div>
+        </div>
+        <div className="headerBarIcons">
+          <div className="barIcon">
+            <Notifications />
+            <span className="barIconCounter">1</span>
+          </div>
+        </div>
+        <img className="headerImg"/>
+      </div>
     </div>
   );
 }
