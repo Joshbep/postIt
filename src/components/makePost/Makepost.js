a3e1a4101f6e3c33d8353b439c8e8ce44ab8d19a
jbep <joshuabemartin@gmail.com>
2022-11-09 20:25:17 -0500

    started styling feed with static things

--- src/components/makePost/Makepost.js
+++ src/components/makePost/Makepost.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import {
+  PermMedia,
+  Label,
+  Room,
+  EmojiEmotions,
+  Cancel
+} from '@material-ui/icons'
 import './makePost.css'
 
 function Makepost() {
   return (
-    <div>
-    hey make post
+    <div className="post">
+      <div className="wrapper">
+        <div className="postTop">
+          <img className="postImage" src="" alt=""/>
+          <input placeholder="What's on your mind?" className="postInput"/>
+        </div>
+        <hr className="postHr" />
+        <div className="postBottom">
+          <div className="postOptions">
+            <div className="option">
+              <PermMedia className="postIcon" />
+              <span className="optionText"> Photo/Video</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
