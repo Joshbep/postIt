e1bc2e94ec99d351a8f6cc2ebf0ef16ce8d149f2
jbep <joshuabemartin@gmail.com>
2022-11-09 20:39:38 -0500

    styled posting component

--- src/components/makePost/Makepost.js
+++ src/components/makePost/Makepost.js
@@ -14,16 +14,29 @@ function Makepost() {
       <div className="wrapper">
         <div className="postTop">
           <img className="postImage" src="" alt=""/>
-          <input placeholder="What's on your mind?" className="postInput"/>
+          <input className="postInput" placeholder="What's on your mind?" />
         </div>
         <hr className="postHr" />
         <div className="postBottom">
           <div className="postOptions">
             <div className="option">
-              <PermMedia className="postIcon" />
+              <PermMedia htmlColor="tomato" className="postIcon" />
               <span className="optionText"> Photo/Video</span>
             </div>
+            <div className="option">
+              <Label htmlColor="blue" className="postIcon" />
+              <span className="optionText"> Tag</span>
+            </div>
+            <div className="option">
+              <Room htmlColor="green" className="postIcon" />
+              <span className="optionText"> Location</span>
+            </div>
+            <div className="option">
+              <EmojiEmotions htmlColor="gold" className="postIcon" />
+              <span className="optionText"> Emojis</span>
+            </div>
           </div>
+          <button className="postButton">Post</button>
         </div>
       </div>
     </div>
