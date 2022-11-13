1513d797085f6ae31c198f887f47df7be4ddb97a
jbep <joshuabemartin@gmail.com>
2022-11-13 17:11:02 -0500

    starting to build login page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import ReactDOM from 'react-dom/client'
 import Home from './pages/home/Home.js'
 import Profile from "./pages/profile/Profile.js"
+import Login from import Profile from "./components/login/Login.js"
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Profile />}/>
+        <Route path='/' element={<Login />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/login/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Login () {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3>POST IT</h3>
+          <span className="LoginDescription">Sign In To POST IT</span>
+        </div>
+        <div className="loginRight">
+          <div className="loginSection">
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
