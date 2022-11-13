c1174f8645500b4a55ef4f1b3b8330fc7ffe2c9b
jbep <joshuabemartin@gmail.com>
2022-11-13 17:26:55 -0500

    adding login inputs and buttons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import ReactDOM from 'react-dom/client'
 import Home from './pages/home/Home.js'
 import Profile from "./pages/profile/Profile.js"
-import Login from import Profile from "./components/login/Login.js"
+import Login from  "./pages/login/Login.js"
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,18 +1,25 @@
 import React from 'react'
+import "./login.css"
 
 function Login () {
   return (
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3>POST IT</h3>
+          <h3 className="loginApp">POST IT</h3>
           <span className="LoginDescription">Sign In To POST IT</span>
         </div>
         <div className="loginRight">
           <div className="loginSection">
+            <input placeHolder="Email" className="loginInput" />
+            <input placeHolder="Password" className="loginInput" />
+            <button className="loginButton">Log in</button>
+            <span className="loginForgot"></span>
           </div>
         </div>
       </div>
     </div>
   )
 }
+
+export default Login;
