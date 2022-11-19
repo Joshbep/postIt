062f97527c3c964b40aeb565feda5fde76e303ea
jbep <joshuabemartin@gmail.com>
2022-11-19 11:00:36 -0500

    started to customize data pulled

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -3,9 +3,11 @@ import {Delete, Favorite} from '@material-ui/icons'
 import './post.css'
 import {useParams} from 'react-router-dom';
 import axios from "axios";
+import {format} from "timeago.js"
+import {Link} from 'react-router-dom'
 
 function Post({post}) {
-  const [like, setLike] = useState(post.likes);
+  const [like, setLike] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState(false);
   const [user, setUser] = useState({});
   let {id} = useParams()
@@ -18,7 +20,7 @@ function Post({post}) {
       setUser(res.data)
     }
     getUsers();
-  }, [])
+  }, [post.userId])
 
   const heartHandler = () => {
     setLike(isLiked ? like-1 : like+1)
@@ -31,9 +33,15 @@ function Post({post}) {
       </div>
         <div className="postsTop">
           <div className="postsTopLeft">
-            <img  className="postsProfileImage" alt="" src={user.profilePicture}/>
+            <Link to={`/profile/${user.username}`}>
+              <img
+              className="postsProfileImage"
+              alt=""
+              src={user.profilePicture || PF+"profile/avatar.png"}
+              />
+            </Link>
             <span className="postsUsername">{user.username}</span>
-            <span className="postsDate">{post.date}</span>
+            <span className="postsDate">{format(post.createdAt)}</span>
           </div>
           <div className="postsTopRight">
             <Delete />
@@ -41,12 +49,16 @@ function Post({post}) {
         </div>
         <div className="postsCenter">
           <span className="postsText">{post?.description}</span>
-          <img className="postsImage" src={PF+post.photo} alt=""/>
+          <img
+          className="postsImage"
+          src={PF + post.img}
+          alt=""
+          />
         </div>
         <div className="postsBottom">
           <div className="postsBottomLeft">
             <Favorite  className="heartIcon" onClick={heartHandler}/>
-            <span className="heartCounter"> {like}</span>
+            <span className="heartCounter"> {like} people like it</span>
           </div>
           <div className="postsBottomRight">
             <span className="postsComment">{post.comments} comments</span>
