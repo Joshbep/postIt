405e6d4e046f6d9783307bc57856156a2fcbb454
jbep <joshuabemartin@gmail.com>
2023-05-12 20:08:28 -0400

    update

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
+let baseURL = ""
+
+if(process.env.NODE_ENV === "development"){
+  baseURL = "http://localhost:3000"
+} else {
+  baseURL = `${process.env.REACT_APP_BACKEND_URL}`
+}
+
 export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("/users/signin", userCredential);
+    const res = await axios.post(baseURL + "/users/signin", userCredential);
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
 
   } catch (err) {
